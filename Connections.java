814f9ccc058df6671695506be666a911bf6bdaca
volker373 <swoodbury@yahoo.com>
2017-04-14 17:17:37 +0000

    Added some basic files

---
+++ Connections.java
@@ -0,0 +1,14 @@
+
+import java.io.File;
+
+public class Connections {
+  
+  public static void connectTo(String address){
+    //TODO
+  }
+  
+  public static void sendFile(File file, String peer){
+    //TODO
+  }
+  
+}
---
+++ GuiMain.java
@@ -0,0 +1,8 @@
+
+public class GuiMain {
+  
+  public static void startGui() {
+    //TODO set up the GUI
+  }
+  
+}
