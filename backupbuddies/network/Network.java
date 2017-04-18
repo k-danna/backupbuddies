d123559cd19e7fc6e158ae55909bbeb0d912ae3e
planetguy <planetguy@ironworks.attlocal.net>
2017-04-18 13:23:48 -0700

    First draft of network code

---
+++ backupbuddies/network/Network.java
@@ -0,0 +1,59 @@
+package backupbuddies.network;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+import backupbuddies.Properties;
+
+public class Network {
+	
+	private final String password;
+	
+	private HashMap<String, Peer> connections;
+	
+	public Network(String password){
+		this.password=password;
+		new Thread(new IncomingConnectionHandler()).start();
+	}
+	
+	public void connect(String url){
+		Peer peer=new Peer(url, password);
+		//We have to lock connections so that no new peer overwrites it
+		synchronized(connections){
+			connections.put(url,peer);
+		}
+	}
+	
+	private class IncomingConnectionHandler implements Runnable {
+
+		//Server socket stays open for the life of the program
+		//We don't care if it leaks
+		@SuppressWarnings("resource")
+		@Override
+		public void run() {
+			ServerSocket serverSocket;
+			try{
+				serverSocket = new ServerSocket(Properties.DEFAULT_PORT);
+			}catch(IOException e){
+				throw(new RuntimeException(e));
+			}
+			
+			while(true)
+			{
+				try {
+					Socket incomingSocket = serverSocket.accept();
+					Peer conn=new Peer(incomingSocket, password);
+					synchronized(connections){
+						connections.put(conn.url, conn);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+}
---
+++ backupbuddies/network/Peer.java
@@ -0,0 +1,89 @@
+package backupbuddies.network;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import backupbuddies.Properties;
+
+public class Peer {
+	
+	private boolean hasFailed=false;
+	
+	private Socket socket;
+	
+	private DataOutputStream outbound;
+	private BufferedReader inbound;
+	
+	public final String url;
+	
+	/**
+	 * Call to create a socket from a client
+	 */
+	Peer(String url, String password){
+		this.url=url;
+		
+		int port = Properties.DEFAULT_PORT;
+		if(url.contains(":")) {
+			port = Integer.parseInt(url.substring(0, url.lastIndexOf(':')));
+			url=url.substring(0, url.lastIndexOf(':'));
+		}
+		try {
+			init(new Socket(url, port));
+			sendHandshake(password);
+		} catch (IOException e) {
+			hasFailed=true;
+		}
+	}
+
+	/**
+	 * Call to create a socket from a server connection only
+	 */
+	Peer(Socket socket, String password) {
+		this.url=socket.getInetAddress().toString();
+		
+		try{
+			init(socket);
+			checkHandshake(password);
+		} catch(Exception e) {
+			hasFailed=true;
+		}
+	}
+	
+	//Sets up the fields
+	private void init(Socket socket) throws IOException{
+		outbound = new DataOutputStream(socket.getOutputStream());
+		inbound = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
+	
+	//Sends a handshake
+	private void sendHandshake(String password) throws IOException {
+		outbound.writeUTF(Properties.HANDSHAKE + "\n");
+		outbound.writeUTF(password+"\n");
+	}
+	
+	//Receives a handshake
+	private boolean checkHandshake(String password) throws IOException {
+		//Check handshake first part
+		String line=inbound.readLine();
+		System.out.println(line);
+		if(!line.equals(Properties.HANDSHAKE))
+			return false;
+		
+		//Check password
+		line=inbound.readLine();
+		System.out.println(line);
+		if(!line.equals(password))
+			return false;
+		
+		//All checks passed = we're good
+		return true;
+	}
+
+	public void 
+	
+	
+
+}
