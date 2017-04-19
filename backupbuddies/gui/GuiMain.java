9d6be6ea1ba13e349aa84e7d87ba745cec68f0c9
Matthew <Matthew@169.233.199.158>
2017-04-19 11:02:03 -0700

    test

--- backupbuddies/gui/GuiMain.java
+++ backupbuddies/gui/GuiMain.java
@@ -1,11 +1,26 @@
 package backupbuddies.gui;
-//did it work?
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 public class GuiMain {
   
   public static void startGui() {
     //TODO set up the GUI
     //pineapple are fum
+	JFrame frame = new JFrame("Hello world!");
+	frame.setSize(500,500);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+	
+	JButton button = new JButton("Click");
+	
+	Container c = 
+	
+	c.add(button, BorderLayout.CENTER);
   }
   
 }
