0651a12ebd2a83f493bff2eb650e8d51e61d9191
kdanna <kdanna@ucsc.edu>
2017-04-19 10:56:29 -0700

    hello world

--- backupbuddies/gui/GuiMain.java
+++ backupbuddies/gui/GuiMain.java
@@ -1,11 +1,20 @@
 package backupbuddies.gui;
 //did it work?
 
+import javax.swing.*;
+
 public class GuiMain {
   
   public static void startGui() {
-    //TODO set up the GUI
-    //pineapple are fum
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("BackupBuddies");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setSize(500, 500);
+                frame.setVisible(true);
+
+            }
+        });
   }
   
 }
