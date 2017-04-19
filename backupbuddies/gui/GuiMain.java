2e911eee28fc90c00b840eb739b1039ff0949fad
mlapo96 <mflapoin@ucsc.edu>
2017-04-19 14:18:56 -0700

    username/pass

--- backupbuddies/gui/GuiMain.java
+++ backupbuddies/gui/GuiMain.java
@@ -17,7 +17,7 @@ import java.awt.*;
 import javax.swing.*;
 
 public class GuiMain {
-  // hi
+  
   public static void startGui() {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
@@ -41,6 +41,24 @@ public class GuiMain {
                 JButton down = new JButton("Download");
                 down.setBounds(500, 550, 175, 50);
                 panel.add(down);
+                
+                // username label/textfield
+                JLabel userLabel = new JLabel("Username:");
+                userLabel.setBounds(10, 10, 150, 25);
+                panel.add(userLabel);
+                
+                JTextField userText = new JTextField(20);
+                userText.setBounds(10, 30, 150, 25);
+                panel.add(userText);
+                
+                // password label/textfield
+                JLabel passLabel = new JLabel("Password:");
+                passLabel.setBounds(10, 50, 150, 25);
+                panel.add(passLabel);
+                
+                JTextField passText = new JTextField(20);
+                passText.setBounds(10, 70, 150, 25);
+                panel.add(passText);
             }
         });
   }
