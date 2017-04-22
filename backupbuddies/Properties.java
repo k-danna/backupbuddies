d167e878150804839723103bb0326a1d20e6b7be
planetguy32 <apricefrench2d@gmail.com>
2017-04-21 18:41:48 -0700

    Added constants for netcode

--- backupbuddies/Properties.java
+++ backupbuddies/Properties.java
@@ -9,6 +9,10 @@ public class Properties {
 	
 	// Handshake constant - if things don't send this after opening,
 	// they're probably not BackupBuddies
-	public static final String HANDSHAKE = "BackupBuddies v0.1"; 
-
+	public static final String HANDSHAKE = "BackupBuddies v0.1";
+	
+	public static final String REQUEST_BACKUP = "B";
+	
+	public static final String REQUEST_RETRIEVE = "R";
+	
 }
